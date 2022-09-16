8efb3ea4f09e1881d62dda187fb5b3956dc89fc0
Robinson Brito <robin_brito@hotmail.com>
2022-09-16 15:47:12 -0500

    commit 3

---
+++ main.js
@@ -0,0 +1,13 @@
+/* DECLARADA */
+function mostrar(){
+    const b = "hola"
+    const c = "andres"
+    let saludo = b +c;
+    return saludo
+}
+
+/* EXPRESADA */
+const muestra = function(){
+
+}
+mostrar()
